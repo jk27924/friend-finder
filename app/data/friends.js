8c9314fd6faf52eab41a3add4fbd5fdda880001d
Jihong Kim <jk279242naver.com>
2019-08-24 18:30:12 -0500

    I changed some pictures and resized them.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,52 +1,52 @@
 module.exports = [
 	{
 		name: "Harry Potter",
-		photo: "https://images.pottermore.com/bxd3o8b291gf/3SQ3X2km8wkQIsQWa02yOY/25f258f21bdbe5f552a4419bb775f4f0/HarryPotter_WB_F4_HarryPotterMidshot_Promo_080615_Port.jpg?w=1200",
+		photo: "https://www.mesaartscenter.com/sysimg/media-box-image-shows-other-presenters-music-of-harry-potter-media-box-14291-image.jpg",
 		answers: [3, 1, 1, 1, 5, 4, 1, 4, 4, 5]
 	},
 	{
 		name: "Ron Weasley",
-		photo: "https://upload.wikimedia.org/wikipedia/en/5/5e/Ron_Weasley_poster.jpg",
+		photo: "https://wizardsunitezone.com/wp-content/uploads/2018/03/Ronald-Weasley-Wallpaper-ronald-weasley-25505490-1024-768-500x500.jpg",
 		answers: [5, 4, 5, 1, 1, 5, 1, 5, 5, 5]
 	},
 	{
 		name: "Hermione Granger",
-		photo: "https://www.google.com/search?q=hermione+granger&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiwguLWx5rkAhVLDq0KHRHvDZoQ_AUIESgB&biw=1315&bih=810#imgrc=7vf_9sUFj5FalM:",
+		photo: "http://images5.fanpop.com/image/photos/31600000/Hermione-hermione-granger-31627751-500-500.png",
 		answers: [4, 1, 2, 1, 5, 5, 1, 5, 5, 5]
 	},
 	{
 		name: "Cho Chang",
-		photo: "https://images.pottermore.com/bxd3o8b291gf/4PbTuJ9S2QoEwycW4gs4Mg/21f94bfe4081438a9bc3c159c2831b5a/ChoChang_WB_F5_ChoChangPointingWand_Promo_080615_Port.jpg?w=1200",
+		photo: "https://pbs.twimg.com/profile_images/766419102/2005_harry_potter_and_the_goblet_of_fire_009.jpg",
 		answers: [5, 1, 4, 1, 5, 5, 1, 5, 5, 5]
 	},
 	{
 		name: "Cedric Diggory",
-		photo: "https://www.google.com/search?tbm=isch&sa=1&ei=Zq5gXdjDMpHktQW81JfABw&q=cedric+diggory&oq=cedric+&gs_l=img.1.0.35i39j0i67j0j0i67j0l2j0i67l3j0.23912.24931..25922...0.0..0.76.470.7......0....1..gws-wiz-img.k03kIfaBUCg#imgrc=uI5AUvYT5ZvqVM:",
+		photo: "https://cdn.theatlantic.com/assets/media/img/mt/2015/09/huff/square.jpg?1522838768",
 		answers: [5, 1, 3, 4, 5, 5, 1, 4, 3, 5]
 	},
 	{
 		name: "Draco Malfoy",
-		photo: "https://upload.wikimedia.org/wikipedia/en/thumb/1/16/Draco_Mal.JPG/220px-Draco_Mal.JPG",
+		photo: "http://images6.fanpop.com/image/photos/32900000/-draco-malfoy-32925350-500-500.png",
 		answers: [4, 2, 4, 4, 5, 4, 1, 5, 5, 5]
 	},
 	{
 		name: "Genie Weasley",
-		photo: "https://www.timewalkertoys.com/v/vspfiles/photos/SA-0063-2.jpg",
+		photo: "https://pbs.twimg.com/profile_images/2955065419/c52ce53c1daa054f1895055dafde4120.jpeg",
 		answers: [4, 1, 1, 1, 3, 1, 1, 5, 2, 5]
 	},
 	{
 		name: "Fleur Delacour",
-		photo: "https://data.whicdn.com/images/315881606/large.jpg",
+		photo: "https://img-ovh-cloud.zszywka.pl/1/0328/3037-fleur-delacour.jpg",
 		answers: [5, 1, 2, 5, 5, 5, 1, 3, 4, 5]
 	},
 	{
 		name: "Victor Krum",
-		photo: "https://vignette.wikia.nocookie.net/harrypotter/images/7/71/Viktorkrumyulleball.png/revision/latest?cb=20120207173113",
+		photo: "http://www.ilpuntoh.com/wp-content/uploads/2016/04/victor1.jpg",
 		answers: [5, 1, 5, 1, 5, 3, 1, 4, 4, 5]
 	},
 	{
 		name: "Neville Longbottom",
-		photo: "https://vignette.wikia.nocookie.net/harry-potter/images/6/6c/Neville_Longbottom_portrait_1.jpg/revision/latest?cb=20090801204451",
+		photo: "https://wizardsunitezone.com/wp-content/uploads/2018/03/Neville-Longbottom-Wallpaper-neville-longbottom-35443295-1024-768-500x500.jpg",
 		answers: [5, 5, 5, 3, 1, 3, 1, 5, 5, 5]
 	}
 ];

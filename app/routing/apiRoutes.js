087a1c4c857941a100f83f1ffbc2da55db09c119
Jihong Kim <jk279242naver.com>
2019-08-23 21:48:55 -0500

    added another function to functions to compare the answers from friends in the list to the new friend.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -18,6 +18,12 @@ module.exports = function(app) {
 
         console.log("\nName: " + friendInput.name + "\nPhoto: " +
         friendInput.photo + "\nScores: " + friendInput.answers);
+
+        friendInput.answers = friendInput.answers.split(",");
+
+		friends.push(friendInput);
+		convertAnswers(friendInput);
+		// console.log(friendInput.answers);
     });
 }
 
@@ -28,3 +34,18 @@ function NewFriend(name, photo, answers) {
 	this.photo = photo;
 	this.answers = answers;
 }
+
+// Function to change formate of survey answers because they come in as strings
+function convertAnswers(currentFriend) {
+	// variable to hold the current friend
+	var current = currentFriend;
+	// console.log(current);
+
+	var curAnswers = current.answers; // holds answers
+
+	// converts answers from strings to numbers
+	for (i=0; i<curAnswers.length; i++) {
+		curAnswers[i] = parseInt(curAnswers[i]);
+	}
+
+} // END convertAnswers()
